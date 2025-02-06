0ee32f773d0ccfc797a42c5cc252fd6657c360b1
Aaron <a.frost@prodware.de>
2025-02-06 10:33:59 +0100

    commit

--- tutorials/02-intermediate/language_model/main.py
+++ tutorials/02-intermediate/language_model/main.py
@@ -17,17 +17,15 @@ embed_size = 128
 hidden_size = 2048
 num_layers = 5
 num_epochs = 10
-num_samples = 500     # number of words to be sampled
+num_samples = 500
 batch_size = 20
 seq_length = 50
 learning_rate = 0.002
 
 corpus = Corpus()
-# Load all text files from a directory
+# load train data
 data_dir = 'C:\\Users\\a.frost\\Desktop\\py\\Pytorch training\\pytorch-tutorial\\tutorials\\02-intermediate\\language_model\\data'
 train_files = [os.path.join(data_dir, file) for file in os.listdir(data_dir) if file.endswith('.txt')]
-
-# Combine data from multiple files
 ids_list = [corpus.get_data(file, batch_size) for file in train_files]
 ids = torch.cat(ids_list, dim=1)
 
@@ -35,7 +33,7 @@ vocab_size = len(corpus.dictionary)
 num_batches = ids.size(1) // seq_length
 
 print("setting up model")
-# RNN based language model
+# model
 class RNNLM(nn.Module):
     def __init__(self, vocab_size, embed_size, hidden_size, num_layers):
         super(RNNLM, self).__init__()
@@ -73,7 +71,7 @@ def load_checkpoint(path):
     # Handle new format
     return checkpoint['model_state_dict'], checkpoint['vocab_size']
 
-# Model initialization and training
+# init and train
 model_path = 'C:\\Users\\aaron\\Desktop\\VSC\\py\\pytorch-tutorial\\model.ckpt'
 if os.path.exists(model_path):
     print('Loading existing model from', model_path)
@@ -92,7 +90,7 @@ if os.path.exists(model_path):
 else:
     print('No existing model found. Training new model...')
     model = RNNLM(vocab_size, embed_size, hidden_size, num_layers).to(device)
-    # Training code here
+    # train
     for epoch in range(num_epochs):
         # Set initial hidden and cell states
         states = (torch.zeros(num_layers, batch_size, hidden_size).to(device),
@@ -126,7 +124,7 @@ else:
                 print ('Epoch [{}/{}], Step[{}/{}], Loss: {:.4f}, Perplexity: {:5.2f}'
                     .format(epoch+1, num_epochs, step, num_batches, loss.item(), np.exp(loss.item())))
 
-# Loss and optimizer
+# Loss and opt 
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
@@ -134,7 +132,7 @@ optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 def detach(states):
     return [state.detach() for state in states]
 
-# Test the model
+# Test
 with torch.no_grad():
     with open('sample.txt', 'w', encoding="utf-8") as f:
         # Set intial hidden ane cell states
